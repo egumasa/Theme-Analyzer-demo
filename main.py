9853c64872b51227210ea2cb1c81004dc9ecb9b5
egumasa <e.masaki0101@gmail.com>
2022-03-04 11:06:27 -0800

    added creative common lisence

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ text = st.sidebar.text_area("",height = 400)
 st.sidebar.caption("""\nTheme Analyzer is still in development. This demo is to demonstrate an automatic approach to Theme Analysis.\n
                    """)
 st.sidebar.markdown("Theme Analyzer is developed by [Masaki Eguchi](https://masakieguchi.weebly.com).")
-
+st.sidebar.markdown('<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.')
 
 rawresult = theme_markup_test(text, nlp, False, True)
 
